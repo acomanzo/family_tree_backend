3a7a0e2f2b6f21adfd0442d81ab860cb90614670
acomanzo <ansonih17@gmail.com>
2021-05-16 18:41:26 -0400

    added some functions for getting a user's shared trees

--- src/controllers/familyTreeController.js
+++ src/controllers/familyTreeController.js
@@ -8,7 +8,28 @@ const family_tree_index = async (req, res) => {
             let pool = await sql.connect(config);
             const result = await pool.request()
                 .input('app_user_id', sql.Int, appUserId)
-                .query('SELECT * FROM FamilyTree WHERE AppUserId = @app_user_id;');
+                .query(
+                    `WITH cte_share_family_tree AS (
+                        SELECT 
+                            FamilyTree.FamilyTreeId, 
+                            FamilyTree.AppUserId, 
+                            FamilyTree.TreeName, 
+                            FamilyTree.CreatedAt, 
+                            FamilyTree.UpdatedAt, 
+                            Share.ShareeId 
+                        FROM FamilyTree INNER JOIN Share ON FamilyTree.FamilyTreeId = Share.FamilyTreeId
+                    )
+                    SELECT 
+                        FamilyTreeId, 
+                        AppUserId, 
+                        TreeName, 
+                        CreatedAt, 
+                        UpdatedAt
+                    FROM cte_share_family_tree WHERE ShareeId = @app_user_id
+                    UNION 
+                    SELECT * FROM FamilyTree WHERE AppUserId = @app_user_id;`
+                )
+                // .query('SELECT * FROM FamilyTree WHERE AppUserId = @app_user_id;');
         
             res.send(result);
         } else {
@@ -46,6 +67,70 @@ const family_tree_create = async (req, res) => {
     }
 };
 
+const family_tree_index_share = async (req, res) => {
+    const appUserId = req.query.appUserId;
+    try {
+        if (appUserId !== undefined) {
+            let pool = await sql.connect(config);
+            const result = await pool.request()
+                .input('sharee_id', sql.Int, appUserId)
+                .query(
+                `WITH cte_share_family_tree AS (
+                    SELECT 
+                        FamilyTree.FamilyTreeId, 
+                        FamilyTree.AppUserId, 
+                        FamilyTree.TreeName, 
+                        FamilyTree.CreatedAt, 
+                        FamilyTree.UpdatedAt, 
+                        Share.ShareeId 
+                    FROM FamilyTree INNER JOIN Share ON FamilyTree.FamilyTreeId = Share.FamilyTreeId
+                )
+                SELECT 
+                    FamilyTreeId, 
+                    AppUserId, 
+                    TreeName, 
+                    CreatedAt, 
+                    UpdatedAt
+                FROM cte_share_family_tree WHERE ShareeId = @sharee_id;`
+            );
+        
+            res.send(result);
+        } else {
+            await sql.connect(config);
+            const result = await sql.query('SELECT * FROM Share;');
+            res.send(result);
+        }
+    } catch (err) {
+        // error checks
+        res.send(err);
+    }
+}
+
+const family_tree_share = async (req, res) => {
+    const appUserId = req.query.appUserId;
+    const familyTreeId = req.query.familyTreeId;
+    const email = req.query.email;
+
+    // get family member id by email
+    try {
+        let pool = await sql.connect(config);
+        const appUserResult = await pool.request()
+            .input('email', sql.VarChar, email)
+            .query('SELECT AppUserId FROM AppUser WHERE Email LIKE @email;');
+        
+        const shareeId = appUserResult.recordsets[0][0].AppUserId;
+
+        const shareResult = await pool.request()
+            .input('sharer_id', sql.Int, appUserId)
+            .input('sharee_id', sql.Int, shareeId)
+            .input('family_tree_id', sql.Int, familyTreeId)
+            .query('INSERT INTO Share (SharerId, ShareeId, FamilyTreeId) OUTPUT INSERTED.* VALUES (@sharer_id, @sharee_id, @family_tree_id);');
+        res.send(shareResult);
+    } catch (err) {
+        res.send(err);
+    }
+}
+
 const family_tree_delete = async (req, res) => {
     const familyTreeId = req.params.id;
 
@@ -65,5 +150,7 @@ const family_tree_delete = async (req, res) => {
 module.exports = {
     family_tree_index, 
     family_tree_create,
+    family_tree_index_share,
+    family_tree_share,
     family_tree_delete
 }
--- src/routes/familyTreeRoutes.js
+++ src/routes/familyTreeRoutes.js
@@ -7,6 +7,10 @@ router.get('/', familyTreeController.family_tree_index);
 
 router.post('/', familyTreeController.family_tree_create);
 
+router.get('/share', familyTreeController.family_tree_index_share);
+
+router.post('/share', familyTreeController.family_tree_share);
+
 router.delete('/:id', familyTreeController.family_tree_delete);
 
 module.exports = router;
